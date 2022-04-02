f1b5146b0d846966d2f38ef86c3ab71fcd87395f
Ray Kim <raykim@Rays-MacBook-Pro.local>
2022-04-02 18:39:17 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import MovieDisplay from "./components/MovieDisplay";
+import Form from "./components/Form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+    const apiKey = "b12b7629";
+
+    const [movie, setMovie] = useState(null);
+
+    const getMovie = async (searchTerm) => {
+        try {
+            const response = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`);
+            const data = await response.json();
+            setMovie(data);
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+    return (
+        <div className="App">
+            <Form moviesearch={getMovie} />
+            <MovieDisplay movie={movie} />
+        </div>
+    )
 }
-
-export default App;
---
+++ src/components/MovieDisplay.js
@@ -0,0 +1,19 @@
+export default function MovieDisplay({ movie }) {
+    const loaded = () => {
+        return (
+            <div className="card">
+                <h1>{movie.Title}</h1>
+                <h2>{movie.Genre}</h2>
+                <img src={movie.Poster} alt={movie.Title} />
+                <h2>{movie.Year}</h2>
+            </div>
+        )
+    }
+
+
+    const loading = () => {
+        return <h1>No Movie to Display</h1>
+    }
+
+    return movie && movie.Title ? loaded() : loading()
+}
